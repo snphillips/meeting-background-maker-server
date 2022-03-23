7de3b5bb87ba2a1330d282a7913a1b2ade8dfe2a
Sarah Phillips <snphillips@gmail.com>
2022-03-23 17:19:29 -0400

    cors

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ const cors = require('cors')
 // app.use(cors())
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://meeting-background-maker.surge.sh/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
