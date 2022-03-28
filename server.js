d8a059dcfaf40f352f80b1c9c7fcb311b5d6b277
Sarah Phillips <snphillips@gmail.com>
2022-03-28 18:38:16 -0400

    response is faster but images still broken on calls with lots of items

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ const Buffer = require('buffer')
 // import the function where we save to AWS S3 bucket
 // const { saveImageToBucket } = require('./s3')
 
-const { processingFunc } = require('./jimpFuncs')
-
-const removeListArray = require('./removeListArray');
+const { processingFunc } = require('./processingFunc')
+const { removeRejects }  = require('./removeRejects');
+// const removeListArray = require('./removeListArray');
 
 // const { template } = require('lodash')
 
@@ -66,184 +66,35 @@ app.get('/alltags/', (req, res, error) => {
 
 app.get('/searchbytag/:value', cors(), (req, res, error) => {
 
-  const { value } = req.params;
-
-  axios({
-    method: 'get',
-    url: `https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.objects&access_token=${process.env.COOPER_API_TOKEN}&has_images=1&per_page=20&tag=${value}`,
-  }).then( (response) => {
+  async function searchByTag() {
+    try {
+      const { value } = req.params;
+      const url = `https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.search.objects&access_token=${process.env.COOPER_API_TOKEN}&has_images=1&per_page=20&tag=${value}`
+      const response = await axios.get(url)
       console.log("HELLO from .then", response.data.objects)
       let tempData = response.data.objects
       
-      // ===========================
-      // Remove Rejects
-      // For various reasons we don't want certain images to display 
-      // like they suck, or are miscategorized).
-      // This function compares images to removeListArray
-      // ===========================
-      function removeRejectList() {
-        let rejectsArray = [];
-        let mergedRejectsArray = [];
-        
-        // 1) create master reject array
-        removeListArray.map( (listItem) => {
-          // smush all the arrays together
-          rejectsArray.push(listItem.removalListId)
-          // now remove the extra array brackets
-          mergedRejectsArray = [].concat.apply([], rejectsArray);
-        })
-        
-        // 2) nested loop over both arrays to look for matches in the
-        // reject array we're making a new temporary array called
-        // keepArray, where we place images we want to keep
-        let keepArray = tempData;
-
-        for (let i = 0; i < tempData.length - 1; i++) {
-          console.log("tempData[i].id", tempData[i].id)
-          for (let j = 0; j < mergedRejectsArray.length; j++) {
-            if (tempData[i].id === mergedRejectsArray[j]) {
-              console.log("a match. DESTROY", tempData[i].id)
-              _Lodash.remove(keepArray, function(item) {
-                return item === tempData[i];
-              });
-              console.log("keepArray.length:", keepArray.length)
-            }
-          }
-        }
-      }
-      removeRejectList()
-
-      
+      await removeRejects(tempData)
       keepArray = tempData
 
-      tempData.map((item) => {
+      await tempData.map((item) => {
         processingFunc(item, value)
       })
 
-      data = _Lodash.compact(tempData)
+      data = await _Lodash.compact(tempData)
 
-    return res.json(data)
-
-  }).catch(function (error) {
-    if (error.response) {
-      console.log("error.response.data:", error.response.data);
-      console.log("error.response.status:", error.response.status);
-      console.log("error.response.headers:", error.response.headers);
-    } else if (error.request) {
-      console.log("error.request:", error.request);
-    } else {
-      console.log('Error:', error.message);
-    }
-    console.log("error.config:", error.config);
-  });
-
-// // =====================================
-// // The big function where we do all kinds of stuff to the 
-// // returned images  
-// // "item" is every item in responseItems, which we're mapping over
-// // =====================================  
-// const processingFunc = (item) => {
-
-//       // TODO: add an error catch
-//       let imageUrl = item.images[0].b.url
-
-//       Jimp.read(imageUrl).then( (meetingBackground) => {
-        
-//           let width = meetingBackground.bitmap.width
-//           let height = meetingBackground.bitmap.height
-//           let aspectRatio = width/height
-//           // console.log("jimp meetingBackground object: ", meetingBackground)
-//           console.log("processing: ", item.id, "width: ", width, "height: ", height)
-
-//         // =========================================
-//         // If the image is too skinny to be an appropriate background, 
-//         // turn it to null (we're going to get rid of it)
-//         function removeSkinnyImages() {
-
-//           if ( (height > width) && ((height / width) > 2 ) ) {
-//             console.log('🧹', item.id, "Skinny PORTRAIT, REMOVE!")
-//             item = null
-//             // _Lodash.remove(responseItems, item)
-//           } else if ( (width > height) && ((width / height) > 2 ) ) {
-//             console.log('🚣‍♀️', item.id, "Skinny LANDSCAPE, REMOVE!")
-//             item = null
-//           } else {
-//             console.log(item.id, "Not skinny. It can stay.")
-//           }
-
-//         }
-//         // troubleshoot reload problem
-//         removeSkinnyImages()
-
-
-//     // =========================================
-
-//     async function imageManipulation() {
-
-//           let widthDim = 1024;
-//           let heightDim = 576;
-//           const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
-          
-//           // If there's no image to manipulate, skip
-//           if (item == null) {
-//             return
-            
-//           } else if (aspectRatio > 1.78) {
-//             console.log(item.id, "LONG LANDSCAPE: bars on top")
-//             // contain
-            
-//           } else if (aspectRatio === 1.78) {
-//             console.log(item.id, "PERFECT LANDSCAPE")
-//             // contain
-            
-//           } else if ((aspectRatio > 1) && (aspectRatio < 1.78)) {
-//             console.log(item.id, "SQUAT LANDSCAPE: bars on side")
-//             // contain
-
-//           } else if (aspectRatio === 1) {
-//             console.log(item.id, "SQUARE: bars on side")
-//             // contain
-
-//           }  else if (aspectRatio < 1) {
-//             console.log(item.id, "PORTRAIT: crop height, bars on side")
-//             // do stuff
-//           }
-
-//           meetingBackground
-//           .autocrop([40, false])
-//           .quality(80) 
-//           // .cover = scale the image to the given width and height,
-//           // (image may be clipped)
-//           // .cover(widthDim, heightDim) // .cover( w, h[, alignBits || mode, mode] );
-//           // .contain = Scale the image to the given width and height,
-//           // (image may be letter boxed)
-//           .contain(widthDim, heightDim)
-//           .background(0x26262626)
-//           .print(font, 10, 536, item.title)
-//           .getBuffer(Jimp.MIME_JPEG, (error, img) => {
-//             if (error) reject(error);
-//             else saveImageToBucket(img, value, item.id)
-//           })
-//         }
-//         imageManipulation()
+      return await res.json(data)
     
-//     // =========================================
+      
 
-//       }).catch(err => {
-//         console.error("Jimp-related server error:", err);
-//       });
 
-//     // The location of where we're saving the image once it's been processed.
-//     // Storing this in the json will make it easier to find on the client
-//     function addLocalImageLocation() {
-//       console.log("💎 inserting image location")
-//       item["imgFileLocation"] = './meeting-backgrounds/' + value + '/' + item.id + '.jpg';
-//     }
-//     addLocalImageLocation()
 
-// };
+    } catch (error) {
+       console.log("error:", error)
+    }
 
-    // =========================================
+  }
+  searchByTag()
 })
 
 
