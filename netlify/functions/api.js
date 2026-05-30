a54c4b30bd6993eb3d09054ea61fb9907b02fabf
Sarah Phillips <snphillips@gmail.com>
2026-05-30 09:19:59 -0400

    update app require path

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,3 +1,3 @@
 const serverless = require('serverless-http');
-const app = require('../../index'); // adjust path to your server entry file
+const app = require('../../server'); // adjust path to your server entry file
 module.exports.handler = serverless(app);
