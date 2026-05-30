0655646bfac1346075ca9c4e5368d70e64e1bf62
Sarah Phillips <snphillips@gmail.com>
2026-05-30 09:54:47 -0400

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,10 +1,6 @@
 const serverless = require('serverless-http');
 const app = require('../../server');
 
-const handler = serverless(app);
-
-module.exports.handler = async (event, context) => {
-  console.log('Incoming path:', event.path);
-  console.log('HTTP method:', event.httpMethod);
-  return handler(event, context);
-};
+module.exports.handler = serverless(app, {
+  basePath: '/.netlify/functions/api'
+});
